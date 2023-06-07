4906efbba3edd47a6a1bf8044d5dbfd3bf7ec10e
fer_sanchez1296 <fernandosanchezplacencia@gmail.com>
2023-06-07 15:33:21 -0600

    Se agrego el controlador para obtener el usuario administador de un laboratorio

--- server/controllers/adminLabsController.js
+++ server/controllers/adminLabsController.js
@@ -1,18 +1,29 @@
 import { pool } from "../db.js";
 
-
 /*Obtenemos todos los maestros de la db*/
-export const getLabs = async(req,res) =>{
-    try {
-        const [result] = await pool.query(`
+export const getLabs = async (req, res) => {
+  try {
+    const [result] = await pool.query(`
         SELECT laboratorio.nombre,laboratorio.edificio,laboratorio.capacidad, CONCAT_WS(" ", usuario.nombre,usuario.apellido_1,
         usuario.apellido_2) as responsable
         FROM laboratorio
         INNER JOIN usuario
         ON laboratorio.usuario_codigo = usuario.codigo;`);
-        res.send(result)
-    } catch (error) {
-        res.send([error.code,error.errno])
-    }
-    
-}
+    res.send(result);
+  } catch (error) {
+    res.send([error.code, error.errno]);
+  }
+};
+
+export const getAdminUser = async (req, res) => {
+  try {
+    const [result] = await pool.query(`
+        SELECT rol.rol AS rol, concat_ws(" ", nombre,apellido_1,apellido_2) AS nombre,codigo,email,telefono 
+        FROM usuario
+        INNER JOIN rol ON usuario.rol_id = rol.id
+        WHERE codigo = (?);`,[req.params.codigo]);
+    res.send(result);
+  } catch (error) {
+    res.send([error.code, error.errno]);
+  }
+};
--- server/routes/adminLabsRoutes.js
+++ server/routes/adminLabsRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import {getLabs} from '../controllers/adminLabsController.js';
+import {getLabs,getAdminUser} from '../controllers/adminLabsController.js';
 
 const router = Router();
 /*Traemos toda la información referente a los laboratórios*/
 router.get('/adminLabs',getLabs);
+
+router.get('/adminLabs-adminUser/:codigo',getAdminUser);
 
 export default router;
