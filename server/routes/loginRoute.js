21cddf8ad39b0159b2132598527662a62e753ef2
fer_sanchez1296 <fernandosanchezplacencia@gmail.com>
2023-06-13 20:43:14 -0600

    .

--- server/routes/loginRoute.js
+++ server/routes/loginRoute.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { login } from "../controllers/loginController.js";
+import { login } from "../controllers/loginController.jsx";
 
 
 const router = Router();
 
-router.get("login/:codigo",login);
+router.get("/login/:codigo",login);
 
 export default router;
