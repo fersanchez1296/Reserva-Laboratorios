356681b278c92eb1b815dcb646f123872c3fe088
fer_sanchez1296 <fernandosanchezplacencia@gmail.com>
2023-06-15 15:17:19 -0600

    .

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -1,16 +1,16 @@
 import { pool } from "../db.js";
 
 export const login = async (req, res) => {
-    const {user , password} = req.params;
+  const { user, password } = req.query;
   try {
     const [result] = await pool.query(
       `
-        SELECT concat_ws(" ", nombre,apellido_1,apellido_2) as nombre, rol.rol
-        from usuario
-        INNER JOIN rol on usuario.rol_id = rol.id
-        WHERE usuario.codigo = ? and usuario.password=?;
-        `,
-      [user,password]
+      SELECT CONCAT_WS(" ", nombre, apellido_1, apellido_2) as nombre, rol.rol
+      FROM usuario
+      INNER JOIN rol ON usuario.rol_id = rol.id
+      WHERE usuario.codigo = ? AND usuario.password = ?;
+      `,
+      [user, password]
     );
     res.send(result);
   } catch (error) {
